2e9706f588346ac6899e41a2684451fe76838f66
Alexis Dumolié <a.dumolie32@gmail.com>
2024-09-24 12:28:55 +0200

    Utilisation de ParameterizedTest pour passer un test plusieurs fois avec des paramètres différents

--- src/main/java/com/alexis/calculator/Calculator.java
+++ src/main/java/com/alexis/calculator/Calculator.java
@@ -2,12 +2,19 @@ package com.alexis.calculator;
 
 public class Calculator {
 
-    public static int add(int opG, int opD){
+//    public static int add(int opG, int opD){
+//        return opG + opD;
+//    }
+//
+//    public static int divide(int opG, int opD){
+//        return opG / opD;
+//    }
+
+    public int add(int opG, int opD){
         return opG + opD;
     }
 
-    public static int divide(int opG, int opD){
+    public int divide(int opG, int opD){
         return opG / opD;
     }
-
 }
--- src/test/java/com/alexis/calculator/CalculatorTest.java
+++ src/test/java/com/alexis/calculator/CalculatorTest.java
@@ -2,31 +2,30 @@ package com.alexis.calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
 
-//    private Calculator calc;
-//
-//    @BeforeEach
-//    public void init(){
-//        calc = new Calculator();
-//    }
-//
-//    @AfterEach
-//    public void teardown() {
-//        calc = null;
-//    }
+    private Calculator calc;
 
-    @Test
-    void methode_add_devrait_retourner_la_somme_de_deux_entiers(){
+    @BeforeEach
+    public void init(){
+        calc = new Calculator();
+    }
 
-        //GIVEN
-        int a = 5;
-        int b = 5;
-        int attendu = 10;
+    @AfterEach
+    public void teardown() {
+        calc = null;
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,1,1","1,2,3","-2,2,0","0,0,0","-1,-2,-3"})
+    void methode_add_devrait_retourner_la_somme_de_deux_entiers(int a, int b, int attendu){
 
         // WITH
-        int resultat = Calculator.add(a,b);
+        int resultat = calc.add(a,b);
 
         // THEN
         assertThat(resultat).isEqualTo(attendu);
@@ -41,7 +40,7 @@ public class CalculatorTest {
         int attendu = 6;
 
         // WITH
-        int resultat = Calculator.divide(a,b);
+        int resultat = calc.divide(a,b);
 
         // THEN
         assertThat(resultat).isEqualTo(attendu);
