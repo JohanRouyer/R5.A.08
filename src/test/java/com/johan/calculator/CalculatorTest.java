62b3b46c47a9e0b3b3036d24388410d7ddc67254
Johan ROUYER <johanlolt@gmail.com>
2024-09-24 11:49:28 +0200

    Tests

--- src/test/java/com/johan/calculator/CalculatorTest.java
+++ src/test/java/com/johan/calculator/CalculatorTest.java
@@ -1,7 +1,61 @@
 package com.johan.calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
+    @Test
+    void additionPositif(){
+        //GIVEN
+        Calculator calculator = new Calculator();
+
+        //WHEN
+        int result = calculator.add(1, 2);
+
+        //THEN
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void additionNegatives(){
+        //given
+        Calculator calculator = new Calculator();
+        //WHEN
+        int result = calculator.add(-1, -2);
+
+        //THEN
+        assertThat(result).isEqualTo(-3);
+    }
+
+    @Test
+    void additionzero(){
+        //given
+        Calculator calculator = new Calculator();
+        //WHEN
+        int result = calculator.add(0,0 );
+
+        //THEN
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void divisionPositif(){
+        //GIVEN
+        Calculator calculator = new Calculator();
+        //WHEN
+        int result = calculator.divide(4,2);
+        //THEN
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void divisionNegative(){
+        //GIVEN
+        Calculator calculator = new Calculator();
+        //WHEN
+        int result = calculator.divide(-4,2);
+        //THEN
+        assertThat(result).isEqualTo(-2);
+    }
 
 }
